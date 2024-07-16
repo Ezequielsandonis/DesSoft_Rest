b58aded1f3b2062ef4f268f176f0a076828a2f44
Ezequiel <ezequielsandonis@gmail.com>
2024-07-16 19:49:26 -0300

    Agregado el ViewModel en la presentacion

---
+++ GestorTareas.Web/Models/TareasViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GestorTareas.Web.Models
+{
+    public class TareasViewModel
+    {
+        public int TareaId { get; set; }
+        public string? Titulo { get; set; }
+        public DateTime? FechaLimite { get; set; }
+        public bool Estado { get; set; }
+    }
+}
